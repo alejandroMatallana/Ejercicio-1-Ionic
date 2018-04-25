a2165418deb87bee25c8ed9be6c3f20399234eb7
Alejandro <alejito.matallana@gmail.com>
2018-04-25 17:50:57 -0500

    primer comit con el ejercicio

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
+	public pet = 'chats';
+	//rootPage = StartPage;
+	//public alertCtrl = AlertController;
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController, public alertCtrl: AlertController) {}
 
-  }
+	showAlert() {
+		let alert = this.alertCtrl.create({
+			title: 'Ocupado',
+			subTitle: 'En este momento no te puedo ayudar',
+			buttons: [ 'OK' ]
+		});
+		alert.present();
+	}
 
+	notificacion() {
+		let alert = this.alertCtrl.create({
+			title: 'Like',
+			subTitle: 'Le has dado me gusta a la foto',
+			buttons: [ 'Continuar' ]
+		});
+		alert.present();
+	}
 }
